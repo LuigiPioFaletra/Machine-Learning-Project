2b841f9fe8e02da9ef3790957dfb2580242cc4a0
Moreno La Quatra <moreno.laquatra@gmail.com>
2024-05-07 16:29:44 +0200

    removed compute_metrics and evaluate from main script

--- test.py
+++ test.py
@@ -11,45 +11,7 @@ from model_classes.ff_model import FFNN
 from yaml_config_override import add_arguments
 from addict import Dict
 
-def compute_metrics(predictions, references):
-    from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
-    acc = accuracy_score(references, predictions)
-    precision = precision_score(references, predictions, average='macro')
-    recall = recall_score(references, predictions, average='macro')
-    f1 = f1_score(references, predictions, average='macro')
-    
-    return {
-        'accuracy': acc,
-        'precision': precision,
-        'recall': recall,
-        'f1': f1
-    }
-
-def evaluate(model, dataloader, criterion, device):
-    model.eval()
-    running_loss = 0.0
-    predictions = []
-    references = []
-    
-    with torch.no_grad():
-        for batch in dataloader:
-            images = batch['image'].to(device)
-            labels = batch['label'].to(device)
-            
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            
-            running_loss += loss.item()
-            
-            pred = torch.argmax(outputs, dim=1)
-            predictions.extend(pred.cpu().numpy())
-            references.extend(labels.cpu().numpy())
-            
-    val_metrics = compute_metrics(predictions, references)
-    val_metrics['loss'] = running_loss / len(dataloader)
-    
-    return val_metrics
+from utils import compute_metrics, evaluate
 
 if __name__ == '__main__':
     # Load configuration
