6d536033634d98ee5cd59e6723217cf30f92c2b0
Luigi Pio Faletra <luigipio.faletra@unikorestudent.it>
2024-08-23 16:00:39 +0200

    Removed 'adaptive average pooling' and updated 'dropout' to 0.1

--- model_classes/ff_model.py
+++ model_classes/ff_model.py
@@ -5,14 +5,12 @@ class FFAudioClassifier(nn.Module):
     def __init__(self, embedding_dim, num_classes):
         super(FFAudioClassifier, self).__init__()
         
-        self.pool = nn.AdaptiveAvgPool1d(embedding_dim)     # Adaptive average pooling layer
         self.fc1 = nn.Linear(embedding_dim, 256)            # First fully connected layer
         self.relu = nn.ReLU()                               # ReLU activation function
-        self.dropout = nn.Dropout(p=0.5)                    # Dropout layer for regularization
+        self.dropout = nn.Dropout(p=0.1)                    # Dropout layer for regularization
         self.fc2 = nn.Linear(256, num_classes)              # Second fully connected layer
         
     def forward(self, x):
-        x = self.pool(x)                # Apply adaptive average pooling to the input tensor
         x = x.view(x.size(0), -1)       # Flatten the tensor from (batch_size, channels, length) to (batch_size, channels * length)
         
         # Pass through the first fully connected layer, followed by ReLU and dropout for regularization
