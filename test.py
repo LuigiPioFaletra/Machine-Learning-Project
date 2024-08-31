14fa0fa143a2b31bc6265d87b69ba53800b44af6
Luigi Pio Faletra <luigipio.faletra@unikorestudent.it>
2024-08-31 18:16:44 +0200

    Modified the structure to be able to test the three proposed models using the configuration of the 'base_config.yaml' file

--- test.py
+++ test.py
@@ -1,55 +1,75 @@
+import joblib
 import os
 import torch
 import torch.nn as nn
 
-from data_classes.fma_dataset import FMADataset
+from addict import Dict
 from model_classes.cnn_model import CNNAudioClassifier
 from model_classes.ff_model import FFAudioClassifier
-from torch.utils.data import DataLoader
+from model_classes.svm_model import SVMAudioClassifier
+from torch.utils.data import DataLoader, TensorDataset
+from train import data_extraction_and_saving
 from utils import evaluate
-
+from yaml_config_override.yaml_config_override import add_arguments
 
 if __name__ == '__main__':
-    # Configuration parameters
-    root = os.path.join(os.path.dirname(__file__), '..', 'fma_small')                 # Path to directory containing audio files
-    metadata_file = os.path.join(os.path.dirname(__file__), '..', 'tracks.csv')       # Path to CSV file with metadata
-    sample_rate = 16000                                                                 # Sample rate for audio data
-    max_duration = 30                                                                   # Maximum duration of audio clips in seconds
-    batch_size = 8                                                                      # Batch size for DataLoader
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")               # Use GPU if available, otherwise use CPU
-    embedding_dim = 768                                                                 # Dimensionality of embeddings (for FF model)
-
-    # Load the test dataset
-    test_ds_cnn = FMADataset(root, metadata_file, 'test', sample_rate, max_duration)    # Test dataset for CNN model
-    test_dl_cnn = DataLoader(test_ds_cnn, batch_size=batch_size, shuffle=False)         # DataLoader for CNN test set
-    test_ds_ff = FMADataset(root, metadata_file, 'test', sample_rate, max_duration)     # Test dataset for FF model
-    test_dl_ff = DataLoader(test_ds_ff, batch_size=batch_size, shuffle=False)           # DataLoader for FF test set
-
-    # Define the model parameters
-    in_channels = 1                             # Number of input channels for CNN
-    out_channels = 512                          # Number of output channels in the first convolutional layer
-    input_length = sample_rate * max_duration   # Length of input audio data
-    kernel_size = 3                             # Size of the convolutional kernel
-    stride = 1                                  # Stride for the convolutional layers
-    padding = 1                                 # Padding for the convolutional layers
-    num_classes = len(test_ds_cnn.genres)       # Number of classes, or genres, for classification
-
-    # Instantiate the models
-    cnn_model = CNNAudioClassifier(in_channels, out_channels, input_length, kernel_size, stride, padding, num_classes).to(device)
-    ff_model = FFAudioClassifier(embedding_dim=embedding_dim, num_classes=num_classes).to(device)
-
-    # Load pre-trained model weights
-    cnn_model.load_state_dict(torch.load("best_cnn_audio_classifier.pth"))      # Load weights for CNN model
-    print("CNN Model loaded.")
-    ff_model.load_state_dict(torch.load("best_ff_audio_classifier.pth"))        # Load weights for FF model
-    print("FF Model loaded.")
-
-    criterion = nn.CrossEntropyLoss()                                           # Define loss function
-    models = [("CNN", cnn_model, test_dl_cnn), ("FF", ff_model, test_dl_ff)]    # Evaluate both models
+    config = Dict(add_arguments())      # Load configuration from a YAML file or command-line arguments
+    device = torch.device('cuda' if torch.cuda.is_available() and config.training.device == 'cuda' else 'cpu')
     
-    for model_name, model, test_dl in models:
-        test_metrics = evaluate(model, test_dl, criterion, device)              # Evaluate the model on the test dataset
+    # Check if test embeddings and labels already exist, otherwise extract and save them
+    if not (os.path.exists(config.filename.test_embeddings) and os.path.exists(config.filename.test_labels)):
+        test_emb, test_lab = data_extraction_and_saving(config.data.dataset_dir,
+                                                        config.data.metadata_file,
+                                                        config.training.sample_rate,
+                                                        config.training.max_duration,
+                                                        config.training.batch_size,
+                                                        config.training.device,
+                                                        config.split.test)
+    # Load pre-saved test embeddings and labels from files
+    else:
+        test_emb = np.load(config.filename.test_embeddings)
+        test_lab = np.load(config.filename.test_labels)
 
-        # Print evaluation metrics
+    # Convert the test embeddings and labels to tensors
+    test_emb_tensor = torch.tensor(test_emb, dtype=torch.float32)
+    test_lab_tensor = torch.tensor(test_lab, dtype=torch.long)
+
+    # Create a TensorDataset for the test data and a DataLoader for batching
+    test_ds = TensorDataset(test_emb_tensor, test_lab_tensor)
+    test_dl = DataLoader(test_ds, config.training.batch_size, shuffle=True, num_workers=0)
+    
+    # Instantiate the CNN and FF models
+    cnn_model = CNNAudioClassifier(config.model.num_classes,
+                                   config.model.dropout).to(device)
+    ff_model = FFAudioClassifier(config.model.input_size,
+                                 config.model.hidden_layers,
+                                 config.model.num_classes,
+                                 config.model.dropout).to(device)
+    svm_model = SVMAudioClassifier()
+
+    # Load the pre-trained model weights for the CNN, FF, and SVM models
+    cnn_model.load_state_dict(torch.load(f'{config.training.checkpoint_dir}/best_model.pt'))
+    print('CNN Model loaded.')
+    ff_model.load_state_dict(torch.load(f'{config.training.checkpoint_dir}/best_model.pt'))
+    print('FF Model loaded.')
+    best_svm = joblib.load(f'{config.training.checkpoint_dir}/best_svm_model.pkl')
+    print('SVM Model loaded.')
+
+    criterion = nn.CrossEntropyLoss()                                   # Define the loss function for evaluation
+    models = [('CNN', cnn_model), ('FF', ff_model)]                     # List of models to evaluate
+    
+    # Evaluate each model on the test dataset
+    for model_name, model in models:
+        test_metrics = evaluate(model, test_dl, criterion, device)      # Evaluate the model on the test dataset
+
+        # Print out the evaluation metrics for each model
         for key, value in test_metrics.items():
-            print(f"{model_name} Model Test {key}: {value:.4f}")
+            print(f'{model_name} Model Test {key}: {value:.4f}')
+
+    ff_model.eval()
+    with torch.no_grad():
+        test_features = ff_model(test_emb_tensor.to(device)).cpu().numpy()
+
+    test_predictions = best_svm.predict(test_features)
+    test_accuracy = np.mean(test_predictions == test_lab)
+    print(f'SVM Model Test Accuracy: {test_accuracy:.4f}')
