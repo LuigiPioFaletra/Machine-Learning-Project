e8d78f90fa1f36f5bb80b1b6c5e0407ae69c0ede
Luigi Pio Faletra <luigipio.faletra@unikorestudent.it>
2024-09-08 18:49:05 +0200

    Added commands for creating the fma_small_data folder and unzipping downloaded .zip files

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,6 @@
 wget https://os.unil.cloud.switch.ch/fma/fma_small.zip
 wget https://os.unil.cloud.switch.ch/fma/fma_metadata.zip
+mkdir fma_small_data
+unzip -q fma_small.zip -d fma_small_data
+unzip -j fma_metadata.zip fma_metadata/tracks.csv -d fma_small_data
 pip install -r requirements.txt
